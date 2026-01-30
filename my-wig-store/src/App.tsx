6ce97141dd9c8f4b0d16f0d8c202534f8abef88c
Hyzeah <hyzeah@Hyzeals-Macbook-Pro.local>
2026-01-30 01:04:21 -0800

    Fix TS config and build for deployment

--- my-wig-store/src/App.tsx
+++ my-wig-store/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import CartSidebar from "./components/CartSidebar";
 import Footer from "./components/Footer";
